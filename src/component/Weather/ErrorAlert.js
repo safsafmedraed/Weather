d9af691d55dd59dbb6eb3ff2d89a58a3d8073dd0
safsafmedraed <mohamedraed.safsaf@esprit.tn>
2022-03-20 17:47:16 +0100

    small refactor

--- src/component/Weather/ErrorAlert.js
+++ src/component/Weather/ErrorAlert.js
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorAlert = () => {
   return (
     <div className="alert alert-primary" role="alert">
-      please Specify the city
+      Please specify the city
     </div>
   );
 };
